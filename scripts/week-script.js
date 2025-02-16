e3cb07d1918d0babce47907ea3a69a560638ba81
ParthTyagi15 <iparth556@gmail.com>
2025-02-16 17:26:08 +0530

    week view starts from Sunday instead of Monday

--- scripts/week-script.js
+++ scripts/week-script.js
@@ -171,8 +171,7 @@ function populateWeek(today) {
 
 function getCurrentWeek(current) {
   let week = new Array();
-  // Starting Monday not Sunday
-  current.setDate(current.getDate() - current.getDay() + 1);
+  current.setDate(current.getDate() - current.getDay());
   for (let i = 0; i < 7; i++) {
     week.push(new Date(current));
     current.setDate(current.getDate() + 1);
